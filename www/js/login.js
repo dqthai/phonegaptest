d10af541522b03d8e37d0dc8c50fa68009986722
Dan Thai <dthai1994@gmail.com>
2015-12-03 21:50:32 -0800

    android key for login

--- www/js/login.js
+++ www/js/login.js
@@ -1,4 +1,4 @@
-Parse.initialize("M0a7TBns2wo7HMdoULhac86LMnpjPothTzst4a1T", "cV4npfDqaSpeTLSwwyhYxg8CvoWqJc0QjXlM37c0");
+Parse.initialize("M0a7TBns2wo7HMdoULhac86LMnpjPothTzst4a1T", "XVHmIAqOUGMNvZoEvKmPTWCtNHvYxQf6Bog5Lvfw");
 //Parse.initialize("3zsVEcWcoiBMqwA4kFftFSJk2kTIsCYY2Hc2dXJ0", "Gp6Mdb5ydKdPiiho32LOFzs5kcwMVqW3pVosxfJy");
 //some useful globals
 var errorText = document.getElementById("errorMessage");
